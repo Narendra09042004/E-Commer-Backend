88930e02f1b78d3787e78388ff037556b2f30e9b
Narendra Parmar <shreenavdurga0099@gmail.com>
2026-04-23 11:24:19 +0530

    surname change

--- ecommerce/controller/AdminController.java
+++ ecommerce/controller/AdminController.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 // RAJU 
-// NARENDRA
+// KARAVADRA
 @RestController
 @Validated
 @RequestMapping("/admin")
